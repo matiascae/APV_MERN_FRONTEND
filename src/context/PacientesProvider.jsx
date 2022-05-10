a4abe521dfac710ca9f61c2470ec172513b02424
matias fontenla <matiasfontenla1994@gmail.com>
2022-05-10 15:55:13 -0300

    mi primer commit

---
+++ src/context/PacientesProvider.jsx
@@ -0,0 +1,114 @@
+import { createContext } from "react";
+import { useState } from "react";
+import { useEffect } from "react";
+import clienteAxios from "../config/axios";
+
+
+const PacientesContext = createContext()
+
+export const PacientesProvider = ({children})=>{
+
+    const[pacientes,setPacientes] = useState([])
+    const[paciente, setPaciente] = useState({})
+
+    useEffect(()=>{
+        const obtenerPacientes = async()=>{
+            try {
+                const token = localStorage.getItem('token')
+                if(!token)
+                return
+                const config = {
+                    headers:{
+                        "Content-Type":"application/json",
+                        Authorization: `Bearer ${token}`
+                    }
+               
+                }
+                const {data} = await clienteAxios('/pacientes',config)
+                setPacientes(data)
+            } catch (error) {
+                console.log(error)
+            }
+        }
+        obtenerPacientes()
+    },[])
+
+    const guardarPaciente = async (paciente)=>{
+
+
+        const token = localStorage.getItem('token')
+        const config = {
+            headers:{
+                "Content-Type":"application/json",
+                Authorization: `Bearer ${token}`
+            }
+        }
+
+        if(paciente.id){
+            try {
+               const {data} = await clienteAxios.put(`/pacientes/${paciente.id}`,paciente,config)
+               const pacienteActualizado  = pacientes.map(oacienteState => pacienteState._id === data._id ? data : pacienteState)
+               setPacientes(pacienteActualizado)
+            } catch (error) {
+                console.log(error)
+            }
+        }else{
+            try {
+                     
+                const {data} = await clienteAxios.post('/pacientes',paciente,config)
+                
+                const{cretedAt, updateAt, __v, ...pacienteAlmacenado}=data
+                
+                setPacientes([pacienteAlmacenado, ...pacientes])
+            
+            } catch (error) {
+                console.log(error.response.data.msg)
+            }
+        }
+
+        
+
+        
+    }
+
+    const setEdicion = (paciente)=>{
+        setPaciente(paciente)
+    }
+
+    const eliminarRegistro = async id=>{
+        const confirmar = confirm('Confirmas que deseas eliminar?')
+        
+        if(confirmar){
+            try {
+                const token = localStorage.getItem('token')
+                const config = {
+                    headers:{
+                        "Content-Type":"application/json",
+                        Authorization: `Bearer ${token}`
+            }
+        }
+                const {data} = await clienteAxios.delete(`/pacientes/${id}`,config)
+                const pacientesActualizado = pacientes.filter(pacientesState => pacienteState._id !== id)
+                setPacientes(pacientesActualizado)
+    }        catch (error) {
+                console.log(error)
+            }
+        }
+    }
+    
+    return(
+        <PacientesContext.Provider
+        value={{
+            pacientes,
+            guardarPaciente,
+            setEdicion,
+            paciente,
+            eliminarRegistro
+        }}>
+            {children}
+        </PacientesContext.Provider>
+    )
+}
+
+
+export default PacientesContext
---
+++ src/layout/AuthLayout.jsx
@@ -0,0 +1,15 @@
+import {Outlet} from 'react-router-dom'
+
+const AuthLayout = () => {
+  return (
+    <>
+       
+
+        <main className='container mx-auto md:grid grid-cols-2 items-center gap-12 p-5'>
+    <Outlet/>
+    </main>
+    </>
+  )
+}
+
+export default AuthLayout
