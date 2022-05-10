5efd78e0d5de42a807ca698b64a5e0d3528611a1
matias fontenla <matiasfontenla1994@gmail.com>
2022-05-10 16:47:23 -0300

    quick fix

--- src/paginas/Login.jsx
+++ src/paginas/Login.jsx
@@ -82,7 +82,7 @@ const Login = () => {
             </div>
             <input 
             type="submit" 
-            value="Crear Cuenta"
+            value="Iniciar Sesion"
             className='bg-indigo-700 w-full py-3 px-10 rounded-xl 
             text-white uppercase font-bold mt-5 hover:cursor-pointer
             hover:bg-indigo-800 md:w-auto' />
